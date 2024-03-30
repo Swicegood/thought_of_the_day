6964b66879fcf7555f1107a6a8c8c834ecb88003
Jagadguru <jguru108@gmail.com>
2024-03-29 20:01:12 -0400

    rename

--- main.py
+++ main.py
@@ -65,11 +65,11 @@ if __name__ == "__main__":
     datestr = parse_email(email_content)
     body = parse_email_for_body(email_content)
     try:
-        body = base64.b64decode(body).decode('utf-8')
+        text = base64.b64decode(body).decode('utf-8')
     except:
-        body = body
+        text = body
     # Insert extracted information into Firestore
-    insert_into_firestore(datestr, body)
+    insert_into_firestore(datestr, text)
 
 
     print("Email processed and data inserted into Firestore.")
